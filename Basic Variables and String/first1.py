1a934153f298775790be5c08aaa356186fbe1b78
HimaniParida <paridahimani2001@gmail.com>
2021-01-03 21:56:45 +0530

    Basic Variables Push

---
+++ Basic Variables and String/first1.py
@@ -0,0 +1,17 @@
+# Variables are used for storage.
+# It is used for saying that allocate memory to it for later use.
+
+# Allocating storage for num1
+num1 = 5
+
+# Allocating storage for num2
+num2 = 10
+
+#Basic Arithmetic and Print
+print("The sum of num1 and num2: ", num1 + num2)
+
+# Changing the value of num1
+num1 = 3
+
+#Printing sum again
+print("After changing num1 sum: ", num1 + num2)
